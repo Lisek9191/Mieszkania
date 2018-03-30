4912737e8c77c64a122ce46df380ac6e41bd2b9b
Lisek9191 <36636711+Lisek9191@users.noreply.github.com>
2018-03-30 11:28:33 +0200

    Add files via upload

---
+++ Scraping podstron.py
@@ -0,0 +1,34 @@
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup as soup
+from time import sleep
+from random import randint
+
+for numer in range(1,102)
+	req=Request('https://www.otodom.pl/sprzedaz/mieszkanie/lodz/?search%5Bdist%5D=0&search%5Bsubregion_id%5D=127&search%5Bcity_id%5D=1004&page='+str(numer), headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'})
+
+
+	sleep(randint(2,4))
+	page=urlopen(req).read()
+
+	urlopen(req).close()
+
+
+	page_soup=soup(page,'html.parser')
+
+	urls=page_soup.findAll("div", {"class":"offer-item-details"})
+
+
+	filename='adresy_url_mieszkań.csv'
+	f=open(filename,'w',encoding="utf-8")
+	headers='adress\n'
+	f.write(headers)
+
+	for url in urls:
+	    adress=url.a['href']
+	    
+
+	    print("'"+adress+"'"+",")
+	    f.write(adress+'\n')
+
+f.close
+
---
+++ sciaganie informacji na temat mieszkania.py
@@ -0,0 +1,80 @@
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup as soup
+
+filename='mieszkanie.txt'
+f=open(filename,'w',encoding="utf-8")
+
+#wklej do "strony" wyniki z programu 'scrapowanie podstron'
+
+strony=['https://www.otodom.pl/oferta/wlasnosciowe-spoldzielcze-37-20-m-bezposrednio-ID3Athp.html#173da81aa9',
+'https://www.otodom.pl/oferta/mieszkanie-3-pokojowe-od-dewelopera-pod-klucz-ID3riOM.html#173da81aa9']
+for strona in strony:
+    
+    req=Request(strona, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'})
+    page=urlopen(req).read()
+
+    urlopen(req).close()
+
+
+    page_soup=soup(page,'html.parser')
+
+
+    try:
+        for z in page_soup('span',{"class":"percentile-location-text"}):
+            osiedle=z.text
+    except IndexError:
+        osiedle=""
+        
+    try:
+        cena=page_soup.findAll('li',{'class':'param_price'})[0].strong.text
+    except IndexError:
+        cena=""
+
+    try:
+        powierzchnia=page_soup.findAll('li',{'class':'param_m'})[0].strong.text
+    except IndexError:
+        powierzchnia=""
+        
+    try:
+        pietro=page_soup.findAll('li',{'class':'param_floor_no'})[0].strong.text
+    except IndexError:
+        pietro=""
+
+    try:
+        for li in page_soup.find_all('li'):
+            if 'Liczba pokoi' in li.text:
+                pokoje = li.text.replace('Liczba pokoi ', '')
+    except IndexError:
+            pokoje=""
+
+    try:
+        informacje=page_soup.findAll('ul',{'class':'sub-list'})[0].text
+    except IndexError:
+        informacje=""
+
+    try:
+        informacje_dodatkowe0=page_soup.findAll('ul',{'class':'dotted-list'})[0].text
+    except IndexError:
+            informacje_dodatkowe0=""
+
+    try:           
+        informacje_dodatkowe1=page_soup.findAll('ul',{'class':'dotted-list'})[1].text
+    except IndexError:
+            informacje_dodatkowe1=""
+                
+    print(osiedle)
+    print(cena)
+    print(powierzchnia)
+    print(pietro)
+    print(pokoje)
+    print(informacje)
+    print(informacje_dodatkowe0)
+    print(informacje_dodatkowe1)
+
+    f.write('INSERT INTO mieszkania VALUES (')
+    f.write("'"+osiedle+"'"+","+"'"+cena+"'"+","+"'"+powierzchnia+"'"+","+"'"+pietro+"'"+","+"'"+pokoje+"'"+","+"'"+informacje+"'"+","+"'"+informacje_dodatkowe0+"'"+","+"'"+informacje_dodatkowe1+"'"+"\n")
+    f.write('); ')
+    
+f.close()
+
+
